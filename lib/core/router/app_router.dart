1869b9500328245b91af56c69cad508459cedd12
Mario Melo <130798353+mariomedev@users.noreply.github.com>
2025-05-20 13:40:22 -0500

    FT: Update analysis options and app dimensions; modify app theme colors and router configuration

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,14 @@
 import 'package:go_router/go_router.dart';
+import 'package:qr_andromeda/features/generete_qr/presentation/screen/generate_qr_screen.dart';
 import 'package:qr_andromeda/features/home/presentation/screen/screen.dart';
 
 final GoRouter goRouter = GoRouter(
   initialLocation: '/home',
-  routes: [GoRoute(path: '/home', builder: (context, state) => HomeSreen())],
+  routes: [
+    GoRoute(path: '/home', builder: (context, state) => const HomeSreen()),
+    GoRoute(
+      path: '/generate_qr',
+      builder: (context, state) => const GenerateQrScreen(),
+    ),
+  ],
 );
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -5,7 +5,7 @@ class AppTheme {
 
   static ThemeData getTheme() => ThemeData(
     colorScheme: ColorScheme.highContrastDark(
-      primary: Color.fromARGB(255, 3, 238, 218),
+      primary: Color.fromARGB(255, 0, 255, 85),
     ),
   );
 }
