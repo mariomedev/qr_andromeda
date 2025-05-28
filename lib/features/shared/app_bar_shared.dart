e9f91e678a98fe4daf9d2338855ee8c97cd0c064
Mario Melo <130798353+mariomedev@users.noreply.github.com>
2025-05-27 22:09:37 -0500

    RF: Update AppBarShared to require title parameter.

--- lib/features/shared/app_bar_shared.dart
+++ lib/features/shared/app_bar_shared.dart
@@ -3,33 +3,37 @@ import 'package:flutter/material.dart';
 import '../../core/core.dart';
 
 class AppBarShared extends StatelessWidget implements PreferredSizeWidget {
-  const AppBarShared({super.key});
+  final String title;
+  const AppBarShared({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    return Container(
-      margin: EdgeInsets.fromLTRB(
-        AppDimensions.kMargin30,
-        AppDimensions.kMargin10,
-        AppDimensions.kMargin30,
-        0,
-      ),
-
-      decoration: BoxDecoration(
-        color: colors.onPrimaryContainer,
-        borderRadius: BorderRadius.circular(6),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.flash_on)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.cameraswitch_sharp)),
-        ],
-      ),
+    return AppBar(
+      title: Text(title),
+      actionsPadding: const EdgeInsets.symmetric(horizontal: 10),
+      actions: [
+        SizedBox(
+          height: 40,
+          width: 40,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: colors.onSecondaryContainer,
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  AppDimensions.kBorderRadius6,
+                ),
+              ),
+            ),
+            onPressed: () {},
+            child: const Icon(Icons.menu_open_sharp),
+          ),
+        ),
+      ],
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(50);
+  Size get preferredSize => const Size.fromHeight(50);
 }
